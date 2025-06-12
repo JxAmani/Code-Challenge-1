7327bbd71a894320576030d6c3b4466aa3a15339
JxAmani <jeremyamani336@gmail.com>
2025-06-12 15:51:46 +0300

    Updating fee.js and it's comments

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -1,27 +1,29 @@
 function estimateTransactionFee() {
-  const amountToSendInput = prompt("Unatuma Ngapi? (KES):");
+  const amountToSendInput = prompt("Unatuma Ngapi? (KES):");//pop-up
   const amountToSend = parseFloat(amountToSendInput);//input into a number
-
+  
+  //Simplified fee structure
   const percentageFeeRate = 0.015;
   const maximumFee = 70;
   const minimumFee = 10;
 
-  if (isNaN(amountToSend) || amountToSend <= 0) {
-    alert("Invalid amount. Please enter a number above 0.");
+  if (isNaN(amountToSend) || amountToSend <= 0) {//If not a number or amount to send is below zero it will ask them put valid amount
+    alert("Invalid amount. Please enter a valid amount.");
     return;
   }
-
+  //caculation for transaction fee
   let transactionFee = amountToSend * percentageFeeRate;
 
+  //Make sure the transaction fee is between minimum fee and maximum fee
   if (transactionFee < minimumFee) {
     transactionFee = minimumFee;
   } else if (transactionFee > maximumFee) {
     transactionFee = maximumFee;
   }
-
+  //caculation for total amount to be debited
   const totalAmountToBeDebited = amountToSend + transactionFee;
 
-  
+  //console the output
   console.log(`Sending KES ${amountToSend}`);
   console.log(`Calculated  Transaction Fee: KES ${transactionFee}`);
   console.log(`Total amount to be debited: KES ${totalAmountToBeDebited}`);
@@ -33,5 +35,5 @@ function estimateTransactionFee() {
   Total amount to be debited: KES ${totalAmountToBeDebited}
 
  Send Money Securely!`);
- 
 }
+estimateTransactionFee();
