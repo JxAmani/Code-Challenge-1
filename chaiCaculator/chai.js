1324f65a8a8b2807e2056f684fdbf66ff340f22d
JxAmani <jeremyamani336@gmail.com>
2025-06-12 15:51:11 +0300

    Updating chai.js and it's comments

--- chaiCaculator/chai.js
+++ chaiCaculator/chai.js
@@ -1,8 +1,9 @@
 function calculateChaiIngredients() {
-  const numberOfCupsInput = prompt("Please, enter the number of chai cups you want to make");
-  const numberOfCups = parseFloat(numberOfCupsInput);
-  if (isNaN(numberOfCups) || numberOfCups <= 0) {
-    console.log(" enter the number of chai cups you want to make ");
+  const numberOfCupsInput = prompt("Please, enter the number of chai cups you want to make");//pop-up
+  const numberOfCups = parseFloat(numberOfCupsInput);//convert input into a number
+
+  if (isNaN(numberOfCups) || numberOfCups <= 0) {//if number of cups of chai is not a number or is below zero it will ask user to put a valid number of cups of chai
+    console.log(" enter a valid number of chai cups you want to make ");
     return ; //exit the function
   }
 
@@ -11,18 +12,21 @@ function calculateChaiIngredients() {
   const milkPerCup = 50; // ml
   const teaLeavesPerCup = 1; // tablespoon
   const sugarPerCup = 2; // teaspoon
-
+  
+  //Caculation for making cups of chai
   const totalWater = numberOfCups * waterPerCup;
   const totalMilk = numberOfCups * milkPerCup;
   const totalTeaLeaves = numberOfCups * teaLeavesPerCup;
   const totalSugar = numberOfCups * sugarPerCup;
 
+  //console output
   console.log(`To make ${numberOfCups} cups of Kenyan Chai, you will need:`);
   console.log(`Water: ${totalWater} ml`);
   console.log(`Milk: ${totalMilk} ml`);
   console.log(`Tea Leaves (Majani): ${totalTeaLeaves} tablespoons`);
   console.log(`Sugar (Sukari): ${totalSugar} teaspoons`);
   console.log("Enjoy your Chai Bora!");
+
   //Added alert because i like the idea of the user getting the output instantly
   alert(`To make ${numberOfCups} cups of Kenyan Chai, you will need
   Water: ${totalWater} ml
@@ -32,8 +36,8 @@ function calculateChaiIngredients() {
 
 
   Enjoy your Chai Bora!`);
-
 }
+calculateChaiIngredients();
 
 
 
