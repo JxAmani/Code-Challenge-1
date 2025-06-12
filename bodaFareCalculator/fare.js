fb322f2569dfe9fea2eeb6d11f097fef9cbd1641
JxAmani <jeremyamani336@gmail.com>
2025-06-12 15:50:42 +0300

    Updating fare.js and it's comments

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,15 +1,17 @@
 function calculateBodaFare() {
-    const distanceInKmInput = prompt("Enter the distance of your trip in kilometers");
+    const distanceInKmInput = prompt("Enter the distance of your trip in kilometers");//pop-up
     const distanceInKm = parseFloat(distanceInKmInput);// input to a number
 
-    if (isNaN(distanceInKm) || distanceInKm <= 0) {
-        console.log("enter the distance of your trip in kilometers")
+    if (isNaN(distanceInKm) || distanceInKm <= 0) {// if input is not a number or distance is below zero it will tell them  to put the correct distance
+        console.log("Kindly put the correct distance")
     }
     const baseFare = 50 ; //KES per ride
     const chargePerKmPer =15 ;// KES for every kilometer traveled.
-
+    
+    //Fare formula
     totalFare = baseFare + (distanceInKm * chargePerKmPer)
     
+    //console output
     console.log(`Uko kwote ni ${distanceInKm}
     Ukikalia Pikipiki: KES ${baseFare}
     Mpaka Uko KES ${chargePerKmPer}
